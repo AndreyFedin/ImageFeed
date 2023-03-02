60858d3b92bd8714d585322d700a8f77965f8258
Andrey Fedin <mondaine.ru@yandex.ru>
2023-03-02 22:54:59 +0300

    Верстка экрана профиля

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by user on 08.02.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
